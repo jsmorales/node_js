fb1700181d683c05a85492f07be509c691705a13
johanmorales <jhellmetal2000@gmail.com>
2017-03-23 12:44:56 -0500

    Se estudia como servir archivos y como clasificar los tipos de archivo dependiendo de su extensión.

--- servidor_http/servidor_http.js
+++ servidor_http/servidor_http.js
@@ -18,21 +18,47 @@ var servidor = http.createServer(function(llamado, respuesta){
 	//la respuesta contiene metodos para escribir el encabezado
 	//de unapagina o para que cuando termine mostrar un resultado
 	
+	//Se muestra cual es el metodo y la url
 	console.log(llamado.method+" -> "+llamado.url)
+	//console.log(llamado.rawHeaders)
 	
+	//Crea el path de llamado con readfile
 	var path = llamado.url == "/" ? "/index.html" : llamado.url;
-
 	var path1 = path.replace("/", "./");
 	
-	fs.readFile(path1, 'utf-8', function(error, contenido){
-		if (error) {
-			respuesta.writeHead(404, {"Content-Type" : "text/html"});
-			respuesta.end("<h1>404!</h1> Esta pagina no existe.");
-		} else {
-			respuesta.writeHead(200, {"Content-Type" : "text/html"});
-			respuesta.end(contenido);
+	
+		/*//El server debe clasificar que tipos de archivo puede servir
+		//y que hacer con cada uno de esos archivos segun su extensión*/
+		
+		if ( (llamado.url.match(/.html$/)) || (llamado.url == "/") ) {
+
+			fs.readFile(path1, 'utf-8', function(error, contenido){
+										
+				if (error) {
+					respuesta.writeHead(404, {"Content-Type" : "text/html"});
+					respuesta.end("<h1>404!</h1> Esta pagina no existe.");
+				}else{
+					respuesta.writeHead(200, {"Content-Type" : "text/html"});
+					respuesta.end(contenido);
+				}
+											
+			})
+
+		}else if(llamado.url.match(/.css$/)){
+
+			fs.readFile(path1, 'utf-8', function(error, contenido){
+				respuesta.writeHead(200, {"Content-Type" : "text/css"});
+				respuesta.end(contenido);
+			})
+
+		}else if(llamado.url.match(/.jpg$/)){
+			
+			fs.readFile(path1, function(error, contenido){
+				respuesta.writeHead(200, {"Content-Type" : "image/jpg"});
+				respuesta.end(contenido);
+			})
 		}
-	})
+
 })
 
 //luego de crear el server se pone a que escuche con los parametros
@@ -42,4 +68,4 @@ servidor.listen(puerto, host,function(){
 	//y genere los resultados.
 	console.log('El server esta corriendo en: '+host+' : '+puerto)
 })
-//killall -9 node
+//Matar proceso en arch --> killall -9 node
